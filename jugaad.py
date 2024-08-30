a4aaefdb0e18c7a03b03adbae1f0ee8e531f5028
sagar sharma <137875419+sagar0023@users.noreply.github.com>
2024-08-30 21:21:58 +0530

    overall_fixed and season added

--- jugaad.py
+++ jugaad.py
@@ -38,7 +38,7 @@ def add_(teams_arr,ipl):
         
     ipl.reset_index(inplace = True)
 
-add_(teams_arr,matches)
+#add_(teams_arr,matches)
 ###########################
 
 ipl_ball = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRu6cb6Pj8C9elJc5ubswjVTObommsITlNsFy5X0EiBY7S-lsHEUqx3g_M16r50Ytjc0XQCdGDyzE_Y/pub?output=csv"
@@ -81,8 +81,10 @@ def allRecord(team):
 def teamAPI(team, matches=matches):
     df = matches[(matches['Team1'] == team) | (matches['Team2'] == team)].copy()
     self_record = allRecord(team)
-    TEAMS = matches.Team1.unique()
-    against = {team2: team1vsteam2(team, team2) for team2 in TEAMS}
+    Teams = matches.Team1.unique().copy()
+    i = np.where(Teams == team)[0][0]
+    Teams = np.delete(Teams,i)
+    against = {team2: team1vsteam2(team, team2) for team2 in Teams}
     data = {team: {'overall': self_record,
                    'against': against}}
     return json.dumps(data, cls=NpEncoder)
