da09856e5f4edf8cbe253054ebfb3064fc674e25
sagar sharma <137875419+sagar0023@users.noreply.github.com>
2024-08-30 21:35:06 +0530

    home msg update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello world"
+    return "Hey this is sagar's api.Keep Hustling :)"
 
 @app.route('/api/teams')
 def teams():
@@ -26,6 +26,22 @@ def teamvteam():
 def team_record():
     team_name = request.args.get('team')
     response = jugaad.teamAPI(team_name)
+    return response
+
+@app.route('/api/season')
+def season_record():
+    current_year = request.args.get('year')
+    if (int(current_year) < 2008) | (int(current_year) > 2022):
+        msg = {'message':'Data not available or invalid year'}
+        return jsonify(msg)
+    
+    if current_year == '2020':
+        current_year = '2020/21'
+    defected_year = ['2010','2008'] #2008 -> 2007/08 2010 -> 2009/10 2020 -> 2020/21
+    if current_year in defected_year:
+        current_year = str(int(current_year)-1)+'/'+current_year[-2:]
+
+    response = season.point_table(current_year)
     return response
 
 if __name__ == '__main__':
