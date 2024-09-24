21519cc3cad62793ac37ba8295f2d7391d7c35b5
sagar sharma <137875419+sagar0023@users.noreply.github.com>
2024-09-24 23:09:14 +0530

    cors eneabling in app

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask,jsonify,request
-import ipl,season
+from flask import Flask, jsonify, request
+from flask_cors import CORS  # Import CORS
+import ipl, season
 import jugaad
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes by default
 
 @app.route('/')
 def home():
-    return "Hey this is sagar's api.Keep Hustling :)"
+    return "Hey this is Sagar's API. Keep Hustling :)"
 
 @app.route('/api/teams')
 def teams():
@@ -18,7 +20,7 @@ def teamvteam():
     team1 = request.args.get('team1')
     team2 = request.args.get('team2')
 
-    response = ipl.teamVteamAPI(team1,team2)
+    response = ipl.teamVteamAPI(team1, team2)
     
     return jsonify(response)
 
@@ -31,17 +33,30 @@ def team_record():
 @app.route('/api/season')
 def season_record():
     current_year = request.args.get('year')
-    if (int(current_year) < 2008) | (int(current_year) > 2022):
-        msg = {'message':'Data not available or invalid year'}
+    if (int(current_year) < 2008) or (int(current_year) > 2022):
+        msg = {'message': 'Data not available or invalid year'}
         return jsonify(msg)
     
     if current_year == '2020':
         current_year = '2020/21'
-    defected_year = ['2010','2008'] #2008 -> 2007/08 2010 -> 2009/10 2020 -> 2020/21
+    
+    defected_year = ['2010', '2008']  # 2008 -> 2007/08 2010 -> 2009/10 2020 -> 2020/21
     if current_year in defected_year:
-        current_year = str(int(current_year)-1)+'/'+current_year[-2:]
+        current_year = str(int(current_year) - 1) + '/' + current_year[-2:]
 
     response = season.point_table(current_year)
+    return response
+
+@app.route('/api/batting-record')
+def batting_record():
+    batsman_name = request.args.get('batsman')
+    response = jugaad.batsmanAPI(batsman_name)
+    return response
+
+@app.route('/api/bowling-record')
+def bowling_record():
+    bowler_name = request.args.get('bowler')
+    response = jugaad.bowlerAPI(bowler_name)
     return response
 
 if __name__ == '__main__':
